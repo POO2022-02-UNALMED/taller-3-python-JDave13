e08d6a8e5a34d536c57068113b6f44ffedeed8cd
JDave13 <111092059+JDave13@users.noreply.github.com>
2022-08-30 10:33:12 -0500

    Intento 1

---
+++ televisores/control.py
@@ -0,0 +1,38 @@
+class Control:
+    
+    def __init__ (self):
+        self._tv = None
+
+    def getTv (self):
+        return self._tv
+
+    def setTv (self, tv):
+        self.tv = tv
+
+    def enlazar (self, tv):
+        self.setTv(tv)
+        self._tv.setControl(self)
+
+    def turnOff (self):
+        self._tv.turnOff()
+    
+    def turnOn (self):
+        self._tv.turnOn()
+    
+    def volumenUp (self):
+        self._tv.volumenUp()
+
+    def volumenDown(self):
+        self._tv.volumenDown()
+    
+    def canalUp (self):
+        self._tv.canalUp()
+        
+    def canalDown (self):
+        self._tv.canalDown()
+
+    def getCanal (self):
+        return self._tv.getCanal()
+    
+    def setCanal (self,cambio):
+        self._tv.setCanal(cambio)
---
+++ televisores/tv.py
@@ -0,0 +1,80 @@
+class TV:
+
+    numTV = 0
+
+    def __init__ (self, marca, estado):
+        self._marca = marca
+        self._estado = estado
+        self._canal = 1
+        self._precio = 500
+        self._volumen = 1
+        self._control = None
+        TV.numTV += 1
+
+    def setMarca (self,marca):
+        self._marca = marca
+    
+    def getMarca (self):
+        return self._marca
+
+    def setControl (self,control):
+        self.control = control
+    
+    def getControl (self):
+        return self.control
+    
+    def setVolumen (self,volumen):
+        if self.getEstado():
+            self._volumen = volumen
+    
+    def getVolumen (self):
+        return self._volumen
+    
+    def setPrecio (self,precio):
+        self._precio = precio
+    
+    def getPrecio (self):
+        return self._precio
+
+    def setCanal (self, canal):
+        if self.getEstado():
+            if (canal>=1 and canal<=120):
+                self._canal = canal
+    
+    def getCanal (self):
+        return self._canal
+
+
+    def turnOn (self):
+        if self._estado == False:
+            self._estado = True
+
+    def turnOff (self):
+        if self._estado == True:
+            self._estado = False
+
+    def getEstado (self):
+        return self._estado
+    
+    def volumenUp (self):
+        if (self.getEstado() == True and self.getVolumen() < 7):
+            self._volumen += 1
+
+    def volumenDown (self):
+        if (self.getEstado() == True and self.getVolumen() > 0):
+            self._volumen -= 1
+
+    def canalUp (self):
+        if (self.getEstado() == True):
+            if (self.getCanal() < 120):
+                self._canal += 1
+            
+    def canalDown (self): 
+        if (self.getEstado() ): 
+            if (self.getCanal() > 1):
+                self._canal -= 1
+    def setNumTV (num):
+        TV.numTV =  num
+    
+    def getNumTV ():
+        return TV.numTV
